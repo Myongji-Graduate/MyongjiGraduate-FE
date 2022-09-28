f4eba85fa507e4519794cea938f9b8cc40a55e42
양성훈 <didtjdgns852@naver.com>
2022-09-28 22:07:22 +0900

    feat server에 hot reload 기능 설정

--- app.js
+++ app.js
@@ -7,9 +7,30 @@ const __dirname = path.resolve();
 
 const app = express();
 
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.client.js');
+
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+
+  const compiler = webpack(webpackConfig);
+
+  app.use(
+    webpackDevMiddleware(compiler, {
+      publicPath: webpackConfig.output.publicPath
+    })
+  );
+
+  app.use(webpackHotMiddleware(compiler));
+}
+
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
+  if (req.url === "/__webpack_hmr") return;
+
   res.send(serverRenderer(req.path))
 })
 
