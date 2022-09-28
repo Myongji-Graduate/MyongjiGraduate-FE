d64f6de767f08c4afd9ab046955ead8ffa5c5107
양성훈 <didtjdgns852@naver.com>
2022-09-29 00:21:47 +0900

    setting responsive 설정

--- src/core/ssr.js
+++ src/core/ssr.js
@@ -5,6 +5,7 @@ export const serverRenderer = (pathname) => `
   <html lang="ko">
   <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Server Side Rendering</title>
   </head>
   <body>
