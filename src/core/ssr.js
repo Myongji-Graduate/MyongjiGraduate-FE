2f73777e1b56dd95e9d1ac22de13ae93ac578a2c
양성훈 <didtjdgns852@naver.com>
2022-10-06 15:24:42 +0900

    feat title 수정

--- src/core/ssr.js
+++ src/core/ssr.js
@@ -6,7 +6,7 @@ export const serverRenderer = (pathname) => `
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Server Side Rendering</title>
+    <title>MJU gradulate</title>
   </head>
   <body>
   <div class="app-container">${router.serverRender(pathname)}</div>
