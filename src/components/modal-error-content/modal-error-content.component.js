cfe4363ba25f3aceafb2aa2940e3c4587f7a0f91
양성훈 <didtjdgns852@naver.com>
2022-10-24 18:01:32 +0900

    feat modal-error-content 컴포넌트 구현

---
+++ src/components/modal-error-content/modal-error-content.component.js
@@ -0,0 +1,21 @@
+import Component from '../../core/component';
+
+import ErrorImage from '../../../public/images/error-image.png';
+
+export default class ModalErrorContent extends Component {
+	template() {
+		return (props) => {
+			if (props) this.setProps(props);
+
+			const { errorMessage } = this.props;
+
+			return `
+        <div class="modal-error-content">
+          <img src=${ErrorImage} class="modal-error-content__error-img" />
+          <div class="modal-error-content__error-message">${errorMessage}</div>
+					<div class="modal-error-content__help-message">도와주세요</div>
+        </div>
+      `;
+		};
+	}
+}
