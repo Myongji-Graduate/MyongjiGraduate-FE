d73790f22b9bf4732d74fbd5b928fdcb6dea8cd6
양성훈 <didtjdgns852@naver.com>
2022-11-11 22:26:41 +0900

    feat 체널톡 제거

--- src/core/ssr-template.js
+++ src/core/ssr-template.js
@@ -20,48 +20,7 @@ const ssrTemplate = (content, isLogin, isInit) => `
   ${isInit ? `window.sessionStorage.setItem('isInit', true);` : ''}
   </script>
   <!-- / csr을 위한 script 태그 추가 -->
-  <!-- Channel Plugin Scripts -->
-  <script>
-    (function() {
-      var w = window;
-      if (w.ChannelIO) {
-        return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
-      }
-      var ch = function() {
-        ch.c(arguments);
-      };
-      ch.q = [];
-      ch.c = function(args) {
-        ch.q.push(args);
-      };
-      w.ChannelIO = ch;
-      function l() {
-        if (w.ChannelIOInitialized) {
-          return;
-        }
-        w.ChannelIOInitialized = true;
-        var s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.async = true;
-        s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
-        s.charset = 'UTF-8';
-        var x = document.getElementsByTagName('script')[0];
-        x.parentNode.insertBefore(s, x);
-      }
-      if (document.readyState === 'complete') {
-        l();
-      } else if (window.attachEvent) {
-        window.attachEvent('onload', l);
-      } else {
-        window.addEventListener('DOMContentLoaded', l, false);
-        window.addEventListener('load', l, false);
-      }
-    })();
-    ChannelIO('boot', {
-      "pluginKey": "7de47477-379e-47dc-9cf3-3b98ddbceee1" 
-    });
-  </script>
-  <!-- End Channel Plugin -->
+  
   
   </body>
   </html>
