4af06d4682f6801f75964248b2d18ff602b3187f
Youkyeong <75975946+youkyeong60@users.noreply.github.com>
2022-11-15 03:42:23 +0900

    chore 오타 수정

--- src/components/result-complete-content/result-complete-content.component.js
+++ src/components/result-complete-content/result-complete-content.component.js
@@ -36,13 +36,13 @@ export default class ResultCompleteContent extends Component {
 				sizes="${backgroundSizeAttr}"
 				srcset="${backgroundSrcsetAttr}"               
                 class="result-complete-content__background" 
-                alt="result-complete-content__background">
+                alt="result-complete-content__background" />
           <div class="result-complete-content__content-container">
             <img 
 			sizes="${checkSizeAttr}"
 			srcset="${checkSrcsetAttr}"        
 			class="result-complete-content__check-icon" 
-			alt="result-complete-content__check-icon">
+			alt="result-complete-content__check-icon" />
             해당 파트의 졸업요건을 충족하셨습니다!
           </div>
         </div>
