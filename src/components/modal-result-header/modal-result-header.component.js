083379218908c06f652e1690028eb37162135610
Youkyeong <75975946+youkyeong60@users.noreply.github.com>
2022-11-22 16:48:25 +0900

    fix toggle 수정

--- src/components/modal-result-header/modal-result-header.component.js
+++ src/components/modal-result-header/modal-result-header.component.js
@@ -1,5 +1,7 @@
 import Component from '../../core/component';
 
+import * as utils from '../../helper/utils';
+
 export default class ModalResultHeader extends Component {
 	setDefaultProps() {
 		this.props = {
@@ -13,6 +15,14 @@ export default class ModalResultHeader extends Component {
 		return (props) => {
 			if (props) this.setProps(props);
 			const { part, completionList, takenCredit, totalCredit } = this.props;
+			const style = {
+				color: completionList ? 'white' : 'gray',
+				left: completionList ? '-2.4rem' : '-1.5rem',
+			};
+
+			const idcheck = document.getElementById('modal-result-header__toggle');
+			// eslint-disable-next-line no-unused-expressions
+			if (idcheck) completionList ? (idcheck.checked = true) : (idcheck.checked = false);
 
 			return `
         <div class="modal-result-header">
@@ -20,31 +30,26 @@ export default class ModalResultHeader extends Component {
                		<div class="modal-result-header__title__part">${part}</div>
 					<div class="modal-result-header__title__explain">
 						${part} 과목 중 &nbsp;
-						<div class="modal-result-header__title__explain__text">${completionList ? `수강` : `미수강`} </div>  
-						한 과목이 표시됩니다.
-						<label class="modal-result-header__togglebox">
-						<span>기수강</span>
-						<input role="switch" type="checkbox" class="modal-result-header__toggle" />
-						<span>미수강</span>
-						</label>						
-				    </div>
-
+						<input role="switch" type="checkbox" class="modal-result-header__toggle" id="modal-result-header__toggle" name='test' value='test' />
+						<div class="modal-result-header__toggle-text" style="${utils.getInlineStyle(style)}">${ completionList ? "기이수" : "미이수" }</div>
+						<div class="modal-result-header__toggle-left">한 과목이 표시됩니다.</div>
+					</div>
 			</div>
-
             <div class="modal-result-header__credit">
-               <div class="modal-result-header__credit__info"> <span>${takenCredit}</span> / ${totalCredit} </div>  
-               <div class="modal-result-header__credit__underline"></div>   
+               <div class="modal-result-header__credit__info"> <span>${takenCredit}</span> / ${totalCredit} </div>
+               <div class="modal-result-header__credit__underline"></div>
             </div>
         </div>
+		
       `;
 		};
 	}
 
 	setEvent() {
 		const { toggleLecture } = this.props;
-		
+
 		this.addEvent('click', '.modal-result-header__toggle', () => {
 			toggleLecture();
-			});
+		});
 	}
 }
